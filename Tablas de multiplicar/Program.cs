1568fadc10292303ee422076030ed59becff6279
Trtinity11k <157337935+Trtinity11k@users.noreply.github.com>
2024-01-21 20:26:29 -0800

    Add files via upload

---
+++ Tablas de multiplicar/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("************************");
+        Console.WriteLine("* Tabla de Multiplicar *");
+        Console.WriteLine("************************");
+
+        Console.WriteLine("*************************************");
+        Console.WriteLine("* Introducir el numero a multiplicar*");
+        Console.WriteLine("*************************************");
+        int a = 0;
+        int Inicio = 0;
+        int Rango = 0;
+        a = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.WriteLine("************************************************");
+        Console.WriteLine("*Ingrese el rango para la tabla de multiplicar:*");
+        Console.WriteLine("************************************************");
+        Rango = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.WriteLine("***************************");
+        Console.WriteLine($"Tabla de Multipliar del: {a}*");
+        Console.WriteLine("***************************");
+        Console.WriteLine();
+        for (int i = 0; i <= Rango; i++)
+        {
+            int resultado = a * i;
+            Console.WriteLine($"{a} x {i} = {resultado}");
+        }
+    }
+}
